74c836081e8e9a4dc5a143b7ff3f929810895d15
thomasZen <thomas.zenkel@googlemail.com>
2024-02-15 17:22:57 +0100

    Small changes

--- openretina/optimization/optimizer.py
+++ openretina/optimization/optimizer.py
@@ -9,14 +9,21 @@ def optimize_stimulus(
         stimulus_regularizing_fn: Callable[[List[torch.Tensor]], torch.Tensor],
         max_iterations: int = 10,
 ) -> None:
+    """
+    Optimize a stimulus to maximize a given objective while minimizing a regularizing function.
+    The stimulus is modified in place.
+    """
     optimizer = optimizer_init_fn(stimulus)
 
-    # when to stop
+    # Could add early stopping interface,
+    # e.g. from [pytorch_lightning](https://lightning.ai/docs/pytorch/stable/common/early_stopping.html)
     for i in range(max_iterations):
         objective = objective.forward(stimulus)
         regularizer_loss = stimulus_regularizing_fn(stimulus)
-        loss = regularizer_loss - objective
+        # Maximizing the objective, minimizing the regularization loss
+        loss = -objective + regularizer_loss
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+    stimulus.detach_()  # Detach the tensor from the computation graph
