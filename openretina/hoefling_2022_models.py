Further fixing numerical stability in main model

openretina/hoefling_2022_models.py
@@ -416,9 +416,10 @@ class SpatialXFeature3d(nn.Module):
         """Gets the actual mask values in terms of a PDF from the mean and SD"""
         self.mask_var_ = torch.exp(self.mask_log_var * self.gaussian_var_scale).view(-1, 1, 1)
         pdf = self.grid - self.mask_mean.view(self.outdims, 1, 1, -1) * self.gaussian_mean_scale
-        pdf = torch.sum(pdf**2, dim=-1) / self.mask_var_
+        pdf = torch.sum(pdf**2, dim=-1) / (self.mask_var_ + 1e-8)
         pdf = torch.exp(-0.5 * pdf)
-        pdf = pdf / torch.sum(pdf, dim=(1, 2), keepdim=True)
+        normalisation = torch.sum(pdf, dim=(1, 2), keepdim=True)
+        pdf = torch.nan_to_num(pdf / normalisation)
         return pdf
 
     def forward(self, x, shift=None, subs_idx=None):