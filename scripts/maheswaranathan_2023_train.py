cb219ef268d238d2a9458008192ad235eb9ecbe7
fededagos <fede.dagos@gmail.com>
2024-02-29 17:45:09 +0100

    Added save_figure convenience function and sample plot at the end of salamander training

--- scripts/maheswaranathan_2023_train.py
+++ scripts/maheswaranathan_2023_train.py
@@ -3,12 +3,15 @@
 import argparse
 import os
 
+import matplotlib.pyplot as plt
 import numpy as np
-
+import seaborn as sns
+import torch
 from openretina.hoefling_2022_configs import model_config, trainer_config
 from openretina.hoefling_2022_data_io import natmov_dataloaders_v2
 from openretina.hoefling_2022_models import SFB3d_core_SxF3d_readout
 from openretina.maheswaranathan_2023_data_io import CLIP_LENGTH, load_all_sessions
+from openretina.plotting import save_figure
 from openretina.training import save_model
 from openretina.training import standard_early_stop_trainer as trainer
 
@@ -46,6 +49,29 @@ def main(data_folder) -> None:
     save_model(
         model=model, save_folder=os.path.join(data_folder, "models"), model_name="SFB3d_core_SxF3d_readout_salamander"
     )
+
+    ## Plotting an example field
+    test_field = "15-10-07"
+    test_sample = next(iter(dataloaders["test"][test_field]))
+
+    input_samples = test_sample.inputs
+    targets = test_sample.targets
+
+    model.eval()
+    model.cpu()
+
+    with torch.no_grad():
+        reconstructions = model(input_samples.cpu(), test_field)
+    reconstructions = reconstructions.cpu().numpy().squeeze()
+
+    targets = targets.cpu().numpy().squeeze()
+    window = 500
+    neuron = 2
+    plt.plot(np.arange(0, window), targets[:window, neuron], label="target")
+    plt.plot(np.arange(30, window + 30), reconstructions[:window, neuron], label="prediction")
+    plt.legend()
+    sns.despine()
+    save_figure(os.path.join(data_folder, "figures"), "salamander_reconstruction_example.pdf")
 
 
 if __name__ == "__main__":
