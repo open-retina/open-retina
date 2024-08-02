8e40571bbacfa19b727e00d6360c53520b69f7fd
Thomas Zenkel <thomas.zenkel@googlemail.com>
2024-08-02 15:30:04 +0200

    Bugfix run script

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+set -ex
 
 for cells in "all" "on" "off" "on-off"; do
     echo "Run training for $cells with Acs"
-    saveFolder="models/rgcs_subsets/all"; mkdir -p ${saveFolder}; ./scripts/hoefling_2022_train.py --data_folder /gpfs01/euler/data/SharedFiles/projects/TP12/ --save_folder $saveFolder --datasets "natural" | tee ${saveFolder}/train.log ;
+    saveFolder="models/rgcs_subsets/${cells}"; mkdir -p ${saveFolder}; ./scripts/hoefling_2022_train.py --data_folder /gpfs01/euler/data/SharedFiles/projects/TP12/ --save_folder $saveFolder --datasets "natural" --cells ${cells} | tee ${saveFolder}/train.log ;
     echo "Run training for $cells without Acs"
-    saveFolder="models/rgcs_subsets/all_no_acs"; mkdir -p ${saveFolder}; ./scripts/hoefling_2022_train.py --data_folder /gpfs01/euler/data/SharedFiles/projects/TP12/ --save_folder $saveFolder --datasets "natural" --max_id 32 | tee ${saveFolder}/train.log ;
+    saveFolder="models/rgcs_subsets/all_no_acs"; mkdir -p ${saveFolder}; ./scripts/hoefling_2022_train.py --data_folder /gpfs01/euler/data/SharedFiles/projects/TP12/ --save_folder $saveFolder --datasets "natural" --cells ${cells} --max_id 32 | tee ${saveFolder}/train.log ;
 done
 
