ffff136e0f48fdf9501bcdc6bfee8e561e56fb12
thomasZen <thomas.zenkel@gmail.com>
2024-08-22 15:26:34 +0200

    Lightning interface for the core readout model (#45)

--- openretina/hoefling_2024/models.py
+++ openretina/hoefling_2024/models.py
@@ -753,7 +753,7 @@ class STSeparableBatchConv3d(nn.Module):
         spatial_kernel_size: int,
         spatial_kernel_size2: int | None = None,
         stride: int = 1,
-        padding: int = 0,
+        padding: int | str | tuple[int, ...] = 0,
         num_scans: int = 1,
         bias: bool = True,
     ):
@@ -798,7 +798,7 @@ class STSeparableBatchConv3d(nn.Module):
         for key, val in log_speed_dict.items():
             setattr(self, key, val)
 
-    def forward(self, input_: tuple[torch.Tensor, str]) -> torch.Tensor:
+    def forward(self, input_: tuple[torch.Tensor, str] | torch.Tensor) -> torch.Tensor:
         """
         Forward pass of the STSeparableBatchConv3d layer.
 
@@ -808,7 +808,11 @@ class STSeparableBatchConv3d(nn.Module):
         Returns:
             torch.Tensor: The output of the convolution.
         """
-        x, data_key = input_
+        if type(input_) is torch.Tensor:
+            x = input_
+            data_key: str | None = None
+        else:
+            x, data_key = input_
 
         # Compute temporal kernel based on the provided data key
         if data_key is None:
