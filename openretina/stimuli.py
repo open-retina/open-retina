66041cdd0668dc0cc7b50f662fcf6129408aa9ea
thomasZen <thomas.zenkel@gmail.com>
2024-10-18 12:57:47 +0200

    Add chirp and moving bar stimuli at 74x64 pixel resolution and 60Hz (#59)

--- openretina/stimuli.py
+++ openretina/stimuli.py
@@ -7,10 +7,10 @@ import numpy as np
 _CURRENT_FOLDER_PATH = os.path.dirname(os.path.abspath(__file__))
 _STIMULUS_FOLDER_PATH = os.path.join(_CURRENT_FOLDER_PATH, "data/stimuli")
 _CHIRP_PATH = _STIMULUS_FOLDER_PATH + "/RGC_Chirp_30Hz_18_16.pickle"
+_CHIRP_72_64_PATH = _STIMULUS_FOLDER_PATH + "/RGC_Chirp_60Hz_72_64.pickle"
 _MOVING_BAR_PATH = _STIMULUS_FOLDER_PATH + "/RGC_MovingBar_30Hz_18_16.pickle"
-_FRAME_RATE = 30  # Hz
-_LENGTH_X = 18
-_LENGTH_Y = 16
+_MOVING_BAR_72_64_PATH = _STIMULUS_FOLDER_PATH + "/RGC_MovingBar_60Hz_72_64.pickle"
+_DEFAULT_FRAME_RATE = 30  # Hz
 
 """
 There are three stimuli this module deals with:
@@ -26,9 +26,9 @@ def normalize_stimulus(stimulus: np.ndarray) -> np.ndarray:
     return normalized
 
 
-def discretize_triggers(trigger_times: np.ndarray) -> list[int]:
+def discretize_triggers(trigger_times: np.ndarray, frame_rate: int = _DEFAULT_FRAME_RATE) -> list[int]:
     assert len(trigger_times.shape) == 1, "trigger_times should be one dimensional"
-    discrete_trigger_times = (trigger_times * _FRAME_RATE).astype(int).tolist()
+    discrete_trigger_times = (trigger_times * frame_rate).astype(int).tolist()
     return discrete_trigger_times
 
 
@@ -61,13 +61,14 @@ def load_stimulus(
     normalize: bool,
     trigger_times: Optional[np.ndarray],
     num_triggers_per_repetition: int,
+    downsample_t_factor: int = 1,
 ) -> np.ndarray:
     with open(file_path, "rb") as f:
         stimulus_uint8 = pickle.load(f)
 
     stimulus = stimulus_uint8.astype(np.float32)
-    # only keep two color channels (which ones does not matter as the moving bar is achromatic)
-    stimulus = stimulus[:, :, :, :2]
+    # only keep two color channels (which ones does not matter as the chirp and moving bars are achromatic)
+    stimulus = stimulus[::downsample_t_factor, :, :, :2]
 
     if trigger_times is not None:
         discrete_trigger_times = discretize_triggers(trigger_times)
@@ -94,6 +95,20 @@ def load_chirp(
     return chirp
 
 
+def load_chirp_30hz_72_64px(
+    normalize: bool = True,
+    trigger_times: Optional[np.ndarray] = None,
+) -> np.ndarray:
+    chirp = load_stimulus(
+        file_path=_CHIRP_72_64_PATH,
+        normalize=normalize,
+        trigger_times=trigger_times,
+        num_triggers_per_repetition=2,
+        downsample_t_factor=2,
+    )
+    return chirp
+
+
 def load_moving_bar(
     normalize: bool = True,
     trigger_times: Optional[np.ndarray] = None,
@@ -109,8 +124,33 @@ def load_moving_bar(
     )
     # Remove the frames that are not part of the moving bar, i.e. black frames at the beginning and end
     # See: https://github.com/eulerlab/QDSpy/blob/master/Stimuli/RGC_MovingBar_2.py
-    frames_before_first_mb = _FRAME_RATE * 3  # 3s before first moving bar
-    frames_after_last_mb = _FRAME_RATE * 1  # 1s after last moving bar
+    frame_rate = 30
+    frames_before_first_mb = frame_rate * 3  # 3s before first moving bar
+    frames_after_last_mb = frame_rate * 1  # 1s after last moving bar
+    moving_bar_content = moving_bar[frames_before_first_mb:-frames_after_last_mb]
+
+    return moving_bar_content
+
+
+def load_moving_bar_30hz_72_64px(
+    normalize: bool = True,
+    trigger_times: Optional[np.ndarray] = None,
+) -> np.ndarray:
+    """Moving bar has 8 triggers, one for each direction.
+    Its directions are [0,180, 45,225, 90,270, 135,315] (in degrees)
+    """
+    moving_bar = load_stimulus(
+        file_path=_MOVING_BAR_72_64_PATH,
+        normalize=normalize,
+        trigger_times=trigger_times,
+        num_triggers_per_repetition=8,
+        downsample_t_factor=2,
+    )
+    # Remove the frames that are not part of the moving bar, i.e. black frames at the beginning and end
+    # See: https://github.com/eulerlab/QDSpy/blob/master/Stimuli/RGC_MovingBar_2.py
+    frame_rate = 30
+    frames_before_first_mb = frame_rate * 3  # 3s before first moving bar
+    frames_after_last_mb = frame_rate * 1  # 1s after last moving bar
     moving_bar_content = moving_bar[frames_before_first_mb:-frames_after_last_mb]
 
     return moving_bar_content
@@ -125,9 +165,16 @@ def load_moving_bar_stack(normalize: bool = True, number_of_moving_bars: int = 8
     return np.stack(np.split(moving_bar, number_of_moving_bars, axis=0), axis=0)
 
 
-def colored_stimulus(channel_idx: int, pad_front: int, stimulus_length: int, pad_end: int) -> np.ndarray:
+def colored_stimulus(
+        channel_idx: int,
+        pad_front: int,
+        stimulus_length: int,
+        pad_end: int,
+        size_x: int,
+        size_y: int,
+) -> np.ndarray:
     total_length_time = pad_front + stimulus_length + pad_end
-    stimulus = np.zeros((2, total_length_time, _LENGTH_X, _LENGTH_Y), dtype=np.float32)
+    stimulus = np.zeros((2, total_length_time, size_x, size_y), dtype=np.float32)
     stimulus[channel_idx, pad_front: pad_front + stimulus_length] = 1.0
     stimulus_5d = np.expand_dims(stimulus, 0)
 
