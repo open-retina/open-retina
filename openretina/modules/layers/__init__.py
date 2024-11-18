c14e000fd4c06406fe667fdeef3cda1dcd08426a
thomasZen <thomas.zenkel@googlemail.com>
2024-11-18 16:25:26 +0100

    codestyle

--- openretina/modules/layers/__init__.py
+++ openretina/modules/layers/__init__.py
@@ -1,4 +1,5 @@
+from openretina.modules.layers.ensemble import EnsembleModel
 from openretina.modules.layers.laplace import FlatLaplaceL23dnorm, Laplace
 from openretina.modules.layers.linear import Bias3DLayer, Scale2DLayer, Scale3DLayer
 
-__all__ = ["FlatLaplaceL23dnorm", "Laplace", "Bias3DLayer", "Scale2DLayer", "Scale3DLayer"]
+__all__ = ["FlatLaplaceL23dnorm", "Laplace", "Bias3DLayer", "Scale2DLayer", "Scale3DLayer", "EnsembleModel"]
