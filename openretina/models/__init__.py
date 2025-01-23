9ad049c1d7c11d0fa81c274bc3b5d8e58efe5b7d
thomasZen <thomas.zenkel@googlemail.com>
2025-01-23 16:55:47 +0100

    Add minimal example to README

---
+++ openretina/models/__init__.py
@@ -0,0 +1,4 @@
+from openretina.models.core_readout import CoreReadout, GRUCoreReadout
+from openretina.models.linear_nonlinear import LNP
+
+__all__ = ["CoreReadout", "GRUCoreReadout", "LNP"]
---
+++ openretina/utils/__init__.py
@@ -0,0 +1,3 @@
+from .file_utils import get_local_file_path
+
+__all__ = ["get_local_file_path"]
