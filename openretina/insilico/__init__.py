968faba46dbb52096fcb787424abc87587d5fdef
thomasZen <thomas.zenkel@googlemail.com>
2025-01-24 10:05:23 +0100

    Plot neuron weights for readout in its own function

--- openretina/insilico/__init__.py
+++ openretina/insilico/__init__.py
@@ -1,9 +1,17 @@
-from openretina.insilico.stimulus_optimization.optimizer import optimize_stimulus
 from openretina.insilico.stimulus_optimization.objective import (
-    SingleNeuronObjective, ContrastiveNeuronObjective,
-    MeanReducer, SliceMeanReducer,
+    ContrastiveNeuronObjective,
+    MeanReducer,
+    SingleNeuronObjective,
+    SliceMeanReducer,
 )
 from openretina.insilico.stimulus_optimization.optimization_stopper import OptimizationStopper
+from openretina.insilico.stimulus_optimization.optimizer import optimize_stimulus
 
-__all__ = ["optimize_stimulus", "SingleNeuronObjective", "ContrastiveNeuronObjective", "OptimizationStopper",
-           "MeanReducer", "SliceMeanReducer"]
+__all__ = [
+    "optimize_stimulus",
+    "SingleNeuronObjective",
+    "ContrastiveNeuronObjective",
+    "OptimizationStopper",
+    "MeanReducer",
+    "SliceMeanReducer",
+]
