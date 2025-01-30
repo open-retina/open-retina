56e757c80ec084d0dd4f454781eeea4cd6581d6b
thomasZen <thomas.zenkel@googlemail.com>
2025-01-30 15:54:16 +0100

    imports in ini

--- openretina/utils/__init__.py
+++ openretina/utils/__init__.py
@@ -1,4 +1,4 @@
 from openretina.utils.file_utils import get_local_file_path
-from openretina.utils.plotting import save_stimulus_to_mp4_video
+from openretina.utils.plotting import save_stimulus_to_mp4_video, plot_stimulus_composition
 
-__all__ = ["get_local_file_path", "save_stimulus_to_mp4_video"]
+__all__ = ["get_local_file_path", "save_stimulus_to_mp4_video", "plot_stimulus_composition"]
