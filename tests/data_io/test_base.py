9f211d01eeda40a5d31417c14c06c556d0a3d151
thomasZen <thomas.zenkel@googlemail.com>
2025-03-27 20:22:05 +0100

    Dev: add data io tests for base classes

---
+++ tests/data_io/test_base.py
@@ -0,0 +1,10 @@
+import pytest
+from openretina.data_io.base import ResponsesTrainTestSplit, MoviesTrainTestSplit
+
+
+def test_movies_train_test_split():
+    assert False
+
+
+def test_responses_train_test_split():
+    assert False
