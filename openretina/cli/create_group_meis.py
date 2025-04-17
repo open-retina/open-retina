e0090167b547b92ae704438130be665eb2bc7d5b
thomasZen <thomas.zenkel@googlemail.com>
2025-04-17 18:00:00 +0200

    Better plot, bugfix

--- openretina/cli/create_group_meis.py
+++ openretina/cli/create_group_meis.py
@@ -56,6 +56,11 @@ def add_parser_arguments(parser: argparse.ArgumentParser):
         help="Whether to generate most depressing input stimuli instead of most exciting ones",
     )
     parser.add_argument(
+        "--center-scale-and-bias",
+        action="store_true",
+        help="Whether to set scale to 1 and bias to 0 for every neuron in the model",
+    )
+    parser.add_argument(
         "--image-file-format", type=str, default="jpg", help="File format to save the visualization plots in"
     )
 
@@ -80,16 +85,26 @@ def visualize_group_meis(
     time_steps_stimulus: int,
     use_smoothing: bool,
     mdi: bool,
+    center_scale_and_bias: bool,
     seed: int,
 ) -> None:
     model = load_core_readout_from_remote(model_path, device=device)
 
     # center all readout to 0.0 to create shared group MEIs
-    center_readout = Center(target_mean=(0.0, 0.0))
+    center_readout = Center(target_mean=(0.0, 0.0), mean_key="mask_mean")
     center_readout(model)
+
+    if center_scale_and_bias:
+        center_scale = Center(1.0, mean_key="scale_param")
+        center_scale(model)
+        center_bias = Center(0.0, mean_key="bias_param")
+        center_bias(model)
+
     model.eval()
     group_assignments = model.get_group_assignments()
-    group_to_neuron_ids = {g: [i for i in group_assignments if i == g] for g in set(group_assignments)}
+    group_to_neuron_ids = {
+        group: [i for i, g in enumerate(group_assignments) if g == group] for group in set(group_assignments)
+    }
 
     stimulus_shape = model.stimulus_shape(time_steps=time_steps_stimulus, num_batches=1)
     min_max_values, norm = _get_min_max_values_and_norm(stimulus_shape[1])
@@ -101,7 +116,7 @@ def visualize_group_meis(
     os.makedirs(save_folder, exist_ok=True)
     for group, neuron_indices in group_to_neuron_ids.items():
         os.makedirs(save_folder, exist_ok=True)
-        print(f"Optimizing group mei for {group=}")
+        print(f"Optimizing group {'mdi' if mdi else 'mei'} for {group=}")
         stimulus_postprocessor_list: list[StimulusPostprocessor] = [stimulus_clipper]
         if use_smoothing:
             stimulus_lowpass_filter = TemporalGaussianLowPassFilterProcessor(sigma=0.5, kernel_size=5, device=device)
@@ -136,10 +151,24 @@ def visualize_group_meis(
         plot_stimulus_composition(
             stimulus=stimulus[0],
             temporal_trace_ax=axes[0, 0],
-            freq_ax=axes[0, 1],
-            spatial_ax=axes[1, 0],
+            spatial_ax=axes[0, 1],
+            freq_ax=axes[1, 1],
             highlight_x_list=[(40, 49)],
         )
+        axes[0, 0].axvline(1.0, ls="--", color="dimgray")  # type:ignore
+
+        # plot traces
+        neuron_traces = model.forward(stimulus)[0, :, neuron_indices].detach().cpu().numpy()
+        neuron_traces_avg = np.average(neuron_traces, axis=-1)
+        ax: plt.Axes = axes[1, 0]  # type: ignore
+        ax.set_title(f"Neuron Responses (n={len(neuron_indices)})")
+        time_axis = np.arange(30, 30 + neuron_traces.shape[0]) / 30.0
+        for trace in np.swapaxes(neuron_traces, 0, 1):
+            ax.plot(time_axis, trace, alpha=0.4)
+        ax.plot(time_axis, neuron_traces_avg, linewidth=4, color="black")
+
+        ax.fill_betweenx(ax.get_ylim(), time_axis[-10], time_axis[-1], color="k", alpha=0.1)
+        ax.axvline(1.0, ls="--", color="dimgray")
 
         input_name = "mdi" if mdi else "mei"
         path_prefix = f"{save_folder}/group_{input_name}_{group:02}"
