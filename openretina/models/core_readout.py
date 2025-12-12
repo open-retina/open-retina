0e36a880b8a1dfb5dbb9debc7e99c4e1d800eabd
thomasZen <thomas.zenkel@gmail.com>
2025-12-12 17:46:00 +0100

    Update openretina/models/core_readout.py

--- openretina/models/core_readout.py
+++ openretina/models/core_readout.py
@@ -425,6 +425,7 @@ class ExampleCoreReadout(BaseCoreReadout):
         super().__init__(core=core, readout=readout, learning_rate=learning_rate, data_info=data_info)
 
     def on_load_checkpoint(self, checkpoint) -> None:
+    """ To support legacy models that use `bias_param` instead of `bias` in their readout layers.
         state_dict = checkpoint["state_dict"]
 
         readout_bias_keys = [k for k in state_dict.keys() if k.startswith("readout.") and k.endswith(".bias_param")]
