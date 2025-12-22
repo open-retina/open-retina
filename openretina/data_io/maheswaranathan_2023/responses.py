b46dfdb22bfbaa35624bdf9cacded9ad0b4a6729
fededagos <fede.dagos@gmail.com>
2025-12-22 12:36:21 +0100

    [WIP] Responses by trial for Maheswaranathan dataset

--- openretina/data_io/maheswaranathan_2023/responses.py
+++ openretina/data_io/maheswaranathan_2023/responses.py
@@ -11,10 +11,45 @@ https://huggingface.co/datasets/open-retina/open-retina/resolve/main/baccus_lab/
 import os
 from typing import Literal
 
+import h5py
+import numpy as np
+from einops import rearrange
+from jaxtyping import Float
+
 from openretina.data_io.base import ResponsesTrainTestSplit
 from openretina.data_io.base_dataloader import NeuronDataSplit
 from openretina.utils.file_utils import get_local_file_path
 from openretina.utils.h5_handling import load_dataset_from_h5
+
+
+def load_test_repeats_for_session(
+    session_path: str | os.PathLike,
+    response_type: Literal["firing_rate_5ms", "firing_rate_10ms", "firing_rate_20ms"] = "firing_rate_20ms",
+    fr_normalization: float = 1,
+) -> Float[np.ndarray, "repeats neurons test_time"]:
+    """
+    Load test response repeats for a single session.
+    """
+
+    with h5py.File(session_path, "r") as f:
+        # There are two possible ways in which the files can be structured
+
+        # 1. Repeats are saved with different response types
+        if response_type in f["test/repeats"].keys():
+            stacked_repeats: Float[np.ndarray, "repeats neurons time"] = f[f"test/repeats/{response_type}"][...]  # type: ignore
+        # 2. Repeats are saved with only one response type, by cell id
+        else:
+            all_test_repeats = {}
+            for cell_id in [x for x in f["test/repeats"].keys() if "cell" in x]:
+                test_repeats = f[f"test/repeats/{cell_id}"][...]  # type: ignore
+                all_test_repeats[cell_id] = test_repeats
+
+            sorted_keys = sorted(all_test_repeats.keys(), key=lambda x: int(x.split("cell")[-1]))
+            stacked_repeats = np.stack([all_test_repeats[key] for key in sorted_keys], axis=0) / fr_normalization
+
+            stacked_repeats = rearrange(stacked_repeats, "neurons repeats time -> repeats neurons time")
+
+    return stacked_repeats
 
 
 def load_all_responses(
@@ -57,6 +92,7 @@ def load_all_responses(
                 train=train_session_data / fr_normalization,
                 test=test_session_data / fr_normalization,
                 stim_id=stim_type,
+                test_by_trial=load_test_repeats_for_session(recording_file, response_type, fr_normalization),
             )
     return responses_all_sessions
 
