5f95bd039589151f7135f9e0717cd87f21ad15c9
thomasZen <thomas.zenkel@gmail.com>
2026-02-20 11:17:51 +0100

    remove correlation

--- openretina/models/core_readout.py
+++ openretina/models/core_readout.py
@@ -109,13 +109,13 @@ class BaseCoreReadout(LightningModule):
         regularization_loss_core = self.core.regularizer()
         regularization_loss_readout = self.readout.regularizer(session_id)  # type: ignore
         total_loss = loss + regularization_loss_core + regularization_loss_readout
-        correlation = -self.validation_loss.forward(model_output, data_point.targets)
+        validation_loss = -self.validation_loss.forward(model_output, data_point.targets)
 
         self.log("regularization_loss_core", regularization_loss_core, on_step=False, on_epoch=True)
         self.log("regularization_loss_readout", regularization_loss_readout, on_step=False, on_epoch=True)
         self.log("train_total_loss", total_loss, on_step=False, on_epoch=True)
         self.log("train_loss", loss, on_step=False, on_epoch=True)
-        self.log("train_correlation", correlation, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("train_validation_loss", validation_loss, on_step=False, on_epoch=True, prog_bar=True)
 
         return total_loss
 
