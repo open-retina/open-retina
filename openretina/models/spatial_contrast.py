01da846c1fe15c5983e727b9bcf92793d33ff53c
shashwatsridhar <shashwat.sridhar.95@gmail.com>
2026-03-19 22:07:02 +0100

    Remove old declaration of w and ensure requires_grad=True for all nonlinearity parameters

--- openretina/models/spatial_contrast.py
+++ openretina/models/spatial_contrast.py
@@ -171,11 +171,9 @@ class SingleCellSpatialContrast(LightningModule):
         self.register_buffer("spatial_filter_sum", torch.tensor(spatial_filter.sum()))
 
         # The 4 learnable parameters
-        self.nl_a = nn.Parameter(torch.tensor(a_init))
-        self.nl_b = nn.Parameter(torch.tensor(b_init))
-        self.nl_c = nn.Parameter(torch.tensor(a_init))
-        self.w = nn.Parameter(torch.tensor(w_init))
-        # The `w` here is different from the `w` used in ParametrizedSoftplus
+        self.nl_a = nn.Parameter(torch.tensor(a_init, dtype=torch.float32), requires_grad=True)
+        self.nl_b = nn.Parameter(torch.tensor(b_init, dtype=torch.float32), requires_grad=True)
+        self.nl_c = nn.Parameter(torch.tensor(a_init, dtype=torch.float32), requires_grad=True)
         self.w = nn.Parameter(torch.tensor(w_init, dtype=torch.float32), requires_grad=True)
 
     def crop_input(self, x: Float[torch.Tensor, "batch channels time height width"]) -> torch.Tensor:
