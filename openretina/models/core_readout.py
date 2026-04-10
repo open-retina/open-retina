239b53805c9e0d5ce53b616b6bb4fabe502dcf6f
thomasZen <thomas.zenkel@gmail.com>
2026-04-10 17:32:22 +0200

    weights_only=False

--- openretina/models/core_readout.py
+++ openretina/models/core_readout.py
@@ -515,10 +515,10 @@ def load_core_readout_from_remote(
     remote_path = _MODEL_NAME_TO_REMOTE_LOCATION.get(model_path, model_path)
     local_path = get_local_file_path(remote_path, cache_directory_path)
     try:
-        return UnifiedCoreReadout.load_from_checkpoint(local_path, map_location=device)
+        return UnifiedCoreReadout.load_from_checkpoint(local_path, map_location=device, weights_only=False)
     except:  # noqa: E722
         # Support for legacy ExampleCoreReadout model
-        return ExampleCoreReadout.load_from_checkpoint(local_path, map_location=device)
+        return ExampleCoreReadout.load_from_checkpoint(local_path, map_location=device, weights_only=False)
 
 
 def load_core_readout_model(
