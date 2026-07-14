47a38b4608370b85d31a2aca03ed6436e34b7dc1
Samuel Suhai <samuel.suhai@uni-tuebingen.de>
2026-07-14 17:51:18 +0200

    Fixed Laplace1d bug

---
+++ tests/models/test_linear_nonlinear.py
@@ -0,0 +1,21 @@
+import pytest
+import torch
+
+from openretina.models.linear_nonlinear import SingleCellSeparatedLNP
+
+@pytest.mark.parametrize("smooth_weight_temp",(0.0,1.0,10.0) )
+def test_single_cell_separated_lnp(smooth_weight_temp: float)  -> None:
+    in_shape = (3,10,15,15)  # "channel time height width"
+    torch.manual_seed(0)
+
+    model = SingleCellSeparatedLNP(in_shape, 
+                                   smooth_weight_temp=smooth_weight_temp,)
+    
+    x = torch.rand((1, *in_shape))
+    x = model.crop_input(x)
+    with torch.no_grad():
+        y = model(x)
+    assert y.shape[0] == 1
+    
+
+    
